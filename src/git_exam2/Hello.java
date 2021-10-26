6cbcd0f2c1c0f980e554caee644cdf6f10136cfa
Jo, Eunji <359303@gmail.com>
2021-10-26 10:09:32 +0900

    201911025-5

--- src/git_exam2/Hello.java
+++ src/git_exam2/Hello.java
@@ -3,9 +3,9 @@ package git_exam2;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("인문융합자율학부");
-		System.out.println("201911025");
-		System.out.println("조은지");
+		System.out.println("소속: 인문융합자율학부");
+		System.out.println("학번: 201911025");
+		System.out.println("이름: 조은지");
 	}
 
 }
